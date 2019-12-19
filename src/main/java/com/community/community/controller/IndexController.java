22aa55277b825921643cc4076e4c3f3760e99bc3
chencanyue <1499047133@qq.com>
2019-12-19 21:30:51 +0800

    add index and bootstrap

---
+++ src/main/java/com/community/community/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
+}

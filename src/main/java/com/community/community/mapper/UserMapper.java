f499e9d54b8c1db97a90599c1999e0f80e7ab38e
chencanyue <1499047133@qq.com>
2019-12-23 22:50:05 +0800

    connect mysql

---
+++ src/main/java/com/community/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.community.community.mapper;
+
+import com.community.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into USER(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}

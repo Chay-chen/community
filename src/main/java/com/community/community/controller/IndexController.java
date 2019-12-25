329a31f9bbfba8ca5c5adef1ff6c7f481b812ba8
chencanyue <1499047133@qq.com>
2019-12-25 23:21:06 +0800

    update nav

--- src/main/java/com/community/community/controller/IndexController.java
+++ src/main/java/com/community/community/controller/IndexController.java
@@ -26,7 +26,7 @@ public class IndexController {
     public String index(HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "10") Integer size){
+                        @RequestParam(name = "size",defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
